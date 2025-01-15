166e23332f6a4408bd17c7929f01093414e062ec
ZhannaGrigoreva <gridj@rambler.ru>
2025-01-15 14:32:29 +0700

    Add files via upload

--- greetings.py
+++ greetings.py
@@ -3,4 +3,9 @@ title = "Defile"
 content = "private_fashion_show"
 status = "waiting"
 created_date = 13-1-2025
+temp_created_date = 13.01
 issue_date = 8-3-2025
+temp_issue_date = 08.03
+print(temp_created_date)
+print(temp_issue_date)
+
