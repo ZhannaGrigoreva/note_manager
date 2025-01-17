fbdba71cf9e49193fbc496405afd254bf17fd19d
ZhannaGrigoreva <gridj@rambler.ru>
2025-01-17 13:37:13 +0700

    Add files via upload

--- add_input.py
+++ add_input.py
@@ -2,6 +2,5 @@ sur_name = input('Введите свое имя: ')
 title = input("Введите название вашего мероприятия: ")
 content = input("Основное содержание вашего мероприятия: ")
 status = "waiting"
-issue_date = input("На какое число запланирована дата показа?: ")
-temp_issue_date = issue_date
-print(temp_issue_date)
+issue_date = input("Введите дату показа? (формат Д-М-Г): ")
+print(issue_date[:5])
