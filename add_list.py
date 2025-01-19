1068588b9374b0ffef677978130df767430f0c1f
ZhannaGrigoreva <gridj@rambler.ru>
2025-01-19 19:27:44 +0700

    Add files via upload

---
+++ add_list.py
@@ -0,0 +1,13 @@
+title1 = input("Введите название 1: ")
+title2 = input('Введите название 2: ')
+title3 = input('Введите название 3: ')
+title4 = input("Введите название 4: ")
+list = [
+    title1, title2,
+    title3, title4
+]
+user_name = "Zhanna_Grigoryeva"
+title = "Defile"
+content = "private_fashion_show"
+status = "waiting"
+print(list, user_name, title, content, status)
