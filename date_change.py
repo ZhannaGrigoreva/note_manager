0cc24c021b1da809b1f431daefe4dec8c3a1d76c
ZhannaGrigoreva <gridj@rambler.ru>
2025-01-20 11:04:19 +0700

    Add files via upload

---
+++ date_change.py
@@ -0,0 +1,8 @@
+user_name = "Zhanna_Grigoryeva"
+title = "Defile"
+content = "private_fashion_show"
+status = "waiting"
+created_date = "13.01.25"
+issue_date = "8.03.25"
+print(created_date[0:5])
+print(issue_date[0:4])
