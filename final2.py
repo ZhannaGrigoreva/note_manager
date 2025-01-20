f9f5fc35f6d02ae0b2331b00d80c239e0673ae02
ZhannaGrigoreva <gridj@rambler.ru>
2025-01-20 11:52:21 +0700

    Add files via upload

---
+++ final2.py
@@ -0,0 +1,14 @@
+list_info = ["user_name = "Zhanna_Grigoryeva"",
+  "title = "Defile"",
+  "content = "private_fashion_show"",
+  "status = "waiting"",
+  "created_date = 13.01.2025",
+  "issue_date = 8.03.2025"
+            ]
+sur_name = input("Введите имя: ")
+content = input("О чем ваша заметка?: ")
+status = input("Каков статус события?: ")
+issue_date = input("На какую дату намечено событие?: ")
+title1 = input("Введите заголовок 1: ")
+title2 = input("Введите заголовок 2: ")
+print(list_info[0])
